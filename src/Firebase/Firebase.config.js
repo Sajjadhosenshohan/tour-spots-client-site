61dbc29446dea248d2871d6c825d536c71deaa34
shohan15020 <mdshohansajjad@gmail.com>
2024-06-14 22:24:26 +0600

    make secure firebase config key

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,19 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyALhSn34JbV-07dh-gSsWrtlltphvIpMUs",
-  // authDomain: "tour-2b613.firebaseapp.com",
-  // projectId: "tour-2b613",
-  // storageBucket: "tour-2b613.appspot.com",
-  // messagingSenderId: "114523960881",
-  // appId: "1:114523960881:web:81ef810c4a37ab612b4710"
-  // ______________ 
-
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
@@ -23,7 +11,6 @@ const firebaseConfig = {
 
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
