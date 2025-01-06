6d297f21d3a25fb0b5314e1b7f8ca6e82b0858f9
Md. Sajjad Hosen Shohan <mdshohansajjad@gmail.com>
2025-01-06 19:06:52 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,75 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   // darkMode: "class",
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       // animation color for text
+//       keyframes: {
+//         animatedgradient: {
+//           '0%': { backgroundPosition: '0% 50%' },
+//           '50%': { backgroundPosition: '100% 50%' },
+//           '100%': { backgroundPosition: '0% 50%' },
+//         },
+//       },
+//       backgroundSize: {
+//         '300%': '300%',
+//       },
+//       animation: {
+//         gradient: 'animatedgradient 6s ease infinite alternate',
+//       },
+//     // 
+//     colors: {
+//       primary: "#f85559",
+//       secondary: "#ffeceb",
+//     }
+//   },
+// },
+// plugins: [require("daisyui")],
+//   daisyui: {
+//   themes: [
+//     'light',
+//     'dark',
+//     'cupcake',
+//     'bumblebee',
+//     'emerald',
+//     'corporate',
+//     'synthwave',
+//     'retro',
+//     'cyberpunk',
+//     'valentine',
+//     'halloween',
+//     'garden',
+//     'forest',
+//     'aqua',
+//     'lofi',
+//     'pastel',
+//     'fantasy',
+//     'wireframe',
+//     'black',
+//     'luxury',
+//     'dracula',
+//     'cmyk',
+//     'autumn',
+//     'business',
+//     'acid',
+//     'lemonade',
+//     'night',
+//     'coffee',
+//     'winter',
+//     'dim',
+//     'nord',
+//     'sunset',
+//   ],
+//   },
+// }
+
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   // darkMode: "class",
   content: [
@@ -21,49 +92,49 @@ export default {
       animation: {
         gradient: 'animatedgradient 6s ease infinite alternate',
       },
-    // 
-    colors: {
-      primary: "#f85559",
-      secondary: "#ffeceb",
-    }
+      colors: {
+        primary: "#f85559",
+        secondary: "#ffeceb",
+      },
+    },
   },
-},
-plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
-  themes: [
-    'light',
-    'dark',
-    'cupcake',
-    'bumblebee',
-    'emerald',
-    'corporate',
-    'synthwave',
-    'retro',
-    'cyberpunk',
-    'valentine',
-    'halloween',
-    'garden',
-    'forest',
-    'aqua',
-    'lofi',
-    'pastel',
-    'fantasy',
-    'wireframe',
-    'black',
-    'luxury',
-    'dracula',
-    'cmyk',
-    'autumn',
-    'business',
-    'acid',
-    'lemonade',
-    'night',
-    'coffee',
-    'winter',
-    'dim',
-    'nord',
-    'sunset',
-  ],
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+    ],
   },
 }
+
 
